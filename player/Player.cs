64d2516dde8dbaa75b538c68ff829ff05af1f498
CrimsonNinja <panruzun@gmail.com>
2023-12-05 00:16:03 +0800

    Add mouse interaction and player selection to Player class
    New mouse interaction signals were added to the Player class in the Player.tscn file to create a more interactive experience. Additionally, a 'choosePlayer' method was introduced to allow changes in player type. These changes will allow greater customization and game control.

--- player/Player.cs
+++ player/Player.cs
@@ -12,11 +12,45 @@ public partial class Player : CharacterBody2D
 	private bool flip = false;
 
 	private bool _canMove = true;
+
+	private bool _stop = false;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_playerAnim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		choosePlayer("player2");
+	}
+
+	public void choosePlayer(string type)
+	{
+		var playerPath = "res://player/assets/";
+		
+		_playerAnim.SpriteFrames.ClearAll();
+		var spriteFrameCustom = new SpriteFrames();
+		spriteFrameCustom.SetAnimationLoop("default",true);
+		spriteFrameCustom.SetAnimationSpeed("default",7.0);
+		var textureSize = new Vector2(960, 240);
+		var spriteSize = new Vector2(240, 240);
+		var fullTexture = GD.Load<Texture2D>(playerPath + type + "/" + type + "-sheet.png");
+
+		var numColumns = (int)(textureSize.X / spriteSize.X);
+		var numRow = (int)(textureSize.Y / spriteSize.Y);
+		
+		for (int x = 0; x < numColumns; x++)
+		{
+			for (int y = 0; y < numRow; y++)
+			{
+				var frmae = new AtlasTexture();
+				frmae.Atlas = fullTexture;
+				var regionVector2 = new Vector2(x, y);
+				frmae.Region = new Rect2((regionVector2 * spriteSize), spriteSize);
+				spriteFrameCustom.AddFrame("default",frmae);
+			}
+		}
+		_playerAnim.SpriteFrames = spriteFrameCustom;
+		
+		_playerAnim.Play("default");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,12 +71,13 @@ public partial class Player : CharacterBody2D
 		_playerAnim.FlipH = flip;
 		
 		_dir = (globalMousePos - selfPos).Normalized();
-		if (_canMove)
+		if (!_canMove || _stop)
 		{
-			Velocity = _dir * _speed;
-			MoveAndSlide();
-			GD.Print(globalMousePos);
+			return;
 		}
+		Velocity = _dir * _speed;
+		MoveAndSlide();
+		GD.Print(globalMousePos);
 	}
 
 	public override void _Input(InputEvent @event)
@@ -60,8 +95,14 @@ public partial class Player : CharacterBody2D
 	
 	private void _OnStopMouseEntered()
 	{
+		_stop = true;
 		// Replace with function body.
 	}
-}
+	
+	private void _OnStopMouseExited()
+	{
+		_stop = false;
+	}
 
+}
 
